a846a2099f3491e7fe82db5a7f54bda64789b792
codenamerohan <officialrohan@gmail.com>
2017-03-17 20:28:25 +0530

    Add files via upload

---
+++ empassistDML.sql
@@ -0,0 +1,14 @@
+INSERT INTO XBBNHCY_USER VALUES (1,'Gangadhar Parde','gangadhar@inautix.co.in','gd','password');
+INSERT INTO XBBNHCY_USER VALUES (2,'Rohan Shankar','rohan@inautix.co.in','rohan','password');
+INSERT INTO XBBNHCY_USER VALUES (3,'Shubham Kumar','shubham@inautix.co.in','shubham','password');
+
+INSERT INTO XBBNHCY_ADMIN VALUES (1,'admin','admin@inautix.co.in','admin','password');
+
+INSERT INTO XBBNHCY_QUESTION VALUES (1,'What is your name?','Let me know your name',1);
+INSERT INTO XBBNHCY_QUESTION VALUES (2,'What is your COMMIT ID?','Let me know your COMMI ID',2);
+
+INSERT INTO XBBNHCY_ANSWER VALUES (1,'Rohan Shankar',2,1);
+INSERT INTO XBBNHCY_ANSWER VALUES (2,'Shubham Kumar',3,1);
+INSERT INTO XBBNHCY_ANSWER VALUES (3,'XBBNHBH',3,2);
+
+INSERT INTO XBBNHCY_VOTES VALUES (1,1,1);
